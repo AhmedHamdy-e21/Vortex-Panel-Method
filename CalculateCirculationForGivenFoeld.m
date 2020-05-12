11a9eb04644dd3b5c0570dec8f456b4496463bbd
Hamdy <s>
2020-05-12 02:41:34 +0200

    Updated Circulation calculation for given velocity field

--- CalculateCirculationForGivenFoeld.m
+++ CalculateCirculationForGivenFoeld.m
@@ -1,48 +1,50 @@
 % % http://www2.math.umd.edu/~jmr/241/lineint1.html#1
 % % http://www2.math.umd.edu/~jmr/241/homework.html
-syms x y z t 
-realdot=@(u,v) u*transpose(v);
 
-F = [y^2,sqrt(4-z^2),x];
+%
 
-tcube= [t,t^2,t^3];
+clear; clc;
 
-Fpar=subs(F,[x,y,z],tcube);     %% so we can substitute for instance by the polar form here
+syms x y z t                                                        %% Define the variables
 
-integrand=realdot(Fpar,diff(tcube,t));   %% I'm happy 
+realdot=@(u,v) u*transpose(v);                                      %% Define the dot product inside the integral
 
-integral=double(int(integrand,t,0,1));    %% int is not an integer however is a integral that takes the variable and limits
-alternate=quadl(@(t) eval(vectorize(integrand)),0,1)                     %% lobatto quadrature integration
+F = [y^2,sqrt(4-z^2),x];                                            %% The function components itself AKA Velocity field
+
+tcube= [t,t^2,t^3];                                                 %% The parameters, or the given contour that we will substitute with
+
+Fpar=subs(F,[x,y,z],tcube);                                         %% Fpar is the parametrized version of the velocity field
+
+integrand=realdot(Fpar,diff(tcube,t));                              %% The integrand inside after we get the dot product
+
+integral=double(int(integrand,t,0,1));                              %% Finally the value of the line integral within the contour (tcube)
+
+alternate=quadl(@(t) eval(vectorize(integrand)),0,1)                %% lobatto quadrature integration, Another way of calculating the integral. We can use Gauss Quadrature.. etc
 
 
 
 
 %%
-%problem 1
-syms x y z t r
-u=y/((x^2)+(y^2));
-v=-x/((x^2)+(y^2));
-G=[u,v];
-unitcircle=[r*cos(t),r*sin(t)]
-
-Gpar=subs(G,[x,y],unitcircle);
-
-integrand=realdot(Gpar,diff(unitcircle,t))
-integral=int(integrand,t,0,2*pi)
-
-% integral=double(int(integrand,t,0,2))
-
-N = 5;   
-X  = linspace(0,N-1,N)';                             % X grid points
-Y  = linspace(0,N-1,N)';   
-[XX,YY] = meshgrid(X,Y);   
-                                                     % Y-axis label
+% Polar parametrization
 
 
-x= 0:0.01:10;
-y= 0:0.01:10;% X grid points
+syms x y  theta r                                                   %% Define the independant variables
 
-plot3(y,x,u);
+u=y/((x^2)+(y^2));                                                  %% the given velocity field in the u direction
+
+v=-x/((x^2)+(y^2));                                                 %% the given velocity field in the v direction
+
+G=[u,v];                                                            %% Total velocity field 
+
+unitcircle=[r*cos(theta),r*sin(theta)]                              %% contour around a unit circle (r is one here) but we realize that the integral value is independant of r
+
+Gpar=subs(G,[x,y],unitcircle);                                      %% The parametrized version of the velocity field (G parametrized)
+
+realdot=@(u,v) u*transpose(v);                                      %% Dot product definition 
+
+integrand=realdot(Gpar,diff(unitcircle,theta));                     %% integrand
+
+integral=-int(integrand,t,0,2*pi)                                   %% the value of circulation ^_^, be careful of the negative sign, it's conventional in circulation to take it CW
 
 
 
---
+++ CirculationForVelocityField.m
@@ -0,0 +1,21 @@
+% we need to define Field 
+% Then define the velocity components 
+% use interpolation to evaluate the velocity field on a given contour(Circle)
+% 
+N= 5;                                           %% number of points 
+
+Vx=rand(N,N);
+Vy=rand(N,N);
+X=linspace(0,N);
+Y=linspace(0,N);
+[XX,YY]=meshgrid(X,Y);
+
+
+
+
+
+
+
+
+
+
