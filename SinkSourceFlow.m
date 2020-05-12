b8521fbcf63a9fff5e14e86c19be2b9bb34b91f7
Hamdy <s>
2020-05-13 01:08:34 +0200

    Simulation the Sink&Source Flow

---
+++ SinkSourceFlow.m
@@ -0,0 +1,22 @@
+Strength=5;
+x0=0;
+y0=0;
+N=10;
+CircleResolution=200;
+X=linspace(-N,N,N);
+Y=linspace(-N,N,N);
+[XX,YY]=meshgrid(X,Y);
+
+for i=1:N
+    for j=1:N
+       x=XX(i,j);
+       y=YY(i,j);
+       dx=x-x0;
+       dy=y-y0;
+       r=sqrt(dx^2+dy^2);
+       Vx(i,j)=-(Strength*dx)/(2*pi*r^2);
+       Vy(i,j)=-(Strength*dy)/(2*pi*r^2); 
+    end
+end
+R=0.1;
+G=CalculateCirculationForField(CircleResolution,R,X,Y,Vx,Vy,x0,y0,N);
